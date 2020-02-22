f3baade63cc45baca8836a1f41eeace4ad0cf655
sami <samistygian@gmail.com>
2020-02-22 21:22:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from 'react';
+import Weather from './components/mainComp.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const key = '5739085cb0a54900837c9c863dc8bae8'; //Api key
+class App extends Component {
+  state = {
+    temperature: '',
+    weather: '',
+    icon: '',
+    description: ''
+  }
+  componentDidMount() {
+    //Fetch data from api
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=${key}`)
+      .then(response => {
+        return response.json();
+      }).then(resp => {
+
+        //Get tomorrow's full date & time
+        const tomorrow = new Date(new Date());
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        let zero = '';
+        if ((tomorrow.getMonth() + 1) < 10) {
+          zero = 0
+        };
+        const FullDate = `${tomorrow.getFullYear()}-${zero}${tomorrow.getMonth() + 1}-${tomorrow.getDate()} 09:00:00`
+
+        //Get the neccessary array with the info for tomorrow
+        let tomorrowData;
+        resp.list.forEach(el => {
+          if (el.dt_txt === FullDate) {
+            return tomorrowData = el;
+          }
+        });
+
+        this.setState({
+          temperature: tomorrowData.main.temp,
+          icon: tomorrowData.weather[0].icon,
+          description: tomorrowData.weather[0].description
+        })
+
+
+      }).catch(console.log)
+  }
+
+  render() {
+    return (
+      <Weather temperature={this.state.temperature}
+        description={this.state.description}
+        icon={this.state.icon}
+      />
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/mainComp.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Weather = (props) => {
+  return (
+    <div className="container">
+      <div className="card text-center">
+        <div className="card-header">
+
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">London's Forecast For Tomorrow at 09:00-12:00</h5>
+          <h1 className="display-1">{props.temperature} °C</h1>
+          <p className='display-4'>{props.description}</p>
+          <img width='100px' height='100px' src={`http://openweathermap.org/img/wn/${props.icon}.png`} alt="" />
+
+        </div>
+        <div className="card-footer text-muted">
+          Have A Nice Day 😊
+        </div>
+      </div>
+    </div>
+
+  )
+};
+
+export default Weather
